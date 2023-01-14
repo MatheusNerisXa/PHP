9b16a04c9bdd5f213b6b31d0eea27817cb211855
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-01-14 15:14:20 -0300

    operacoes

---
+++ PHP: conceitos, lidando com dados, loops e mais/operacoes.php
@@ -0,0 +1,14 @@
+<?php
+$idade = 25;
+$idadeDaqui10Anos = $idade + 10;
+$idadeA5Anos = $idade - 5;
+//echo $idadeDaqui10Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 8;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+$restoDaDivisao =  10 % 3;
+echo $restoDaDivisao;
