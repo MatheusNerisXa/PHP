5ad2f9cde0845985972262fd8e7216fcebd7f2f3
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-01-15 15:52:27 -0300

    html com php

--- Arrays-strings-funcoes-e-web/Aulas/banco.php
+++ Arrays-strings-funcoes-e-web/Aulas/banco.php
@@ -20,14 +20,34 @@ titularComLetrasMaiusculas($contasCorrentes['443.931.950-51']);
 
 $contasCorrentes['720.067.870-80'] = depositar(
     $contasCorrentes['720.067.870-80'],
-    -200
+    200
 );
 
 $contasCorrentes['907.627.830-01'] = sacar($contasCorrentes['907.627.830-01'],300);
-unset($contasCorrentes['907.627.830-01']);
+//unset($contasCorrentes['907.627.830-01']);
 $contasCorrentes['443.931.950-51'] = sacar($contasCorrentes['443.931.950-51'],100);
+?>
 
-foreach ($contasCorrentes as $cpf => $conta){
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    echo  exibeMensagem("$cpf $titular $saldo");
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta){ ?>
+        <dt>
+            <h3><?=$conta['titular'];?> - <?=$cpf;?></h3>
+        </dt>
+        <dd>
+            Saldo: <?=$conta['saldo'];?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
