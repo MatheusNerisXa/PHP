6c7fdfbf921b6d971ca0778c355f622c84cdc11b
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-02-06 17:54:14 -0300

    PHP I/O

--- Io-arquivos-streams/Aulas/arquivos-para-csv.php
+++ Io-arquivos-streams/Aulas/arquivos-para-csv.php
@@ -7,12 +7,12 @@ $outrosCursos = file('cursos-php.txt');
 $arquivoCsv = fopen('cursos.csv', 'w');
 
 foreach ($meusCursos as $curso){
-    $linha = [trim($curso), "Sim"];
+    $linha = [trim(utf8_decode($curso)), "Sim"];
     fputcsv($arquivoCsv,$linha, ';');
 }
 
 foreach ($outrosCursos as $curso){
-    $linha = [trim($curso), "Não"];
+    $linha = [trim(utf8_decode($curso)), "Não"];
     fputcsv($arquivoCsv,$linha, ';');
 }
 
