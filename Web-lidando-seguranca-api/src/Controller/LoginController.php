052268f7f629d8e5558c7c1f96f0d52ec3eae8a5
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-02-15 07:08:07 -0300

    autorizacao com session

--- Web-lidando-seguranca-api/src/Controller/LoginController.php
+++ Web-lidando-seguranca-api/src/Controller/LoginController.php
@@ -26,6 +26,7 @@ class LoginController implements Controller
         $correctPassword = password_verify($password, $userData['password'] ?? '');
 
         if ($correctPassword) {
+            $_SESSION['logado'] = true;
             header('Location: /');
         } else {
             header('Location: /login?sucesso=0');
--- Web-lidando-seguranca-api/src/Controller/LoginFormController.php
+++ Web-lidando-seguranca-api/src/Controller/LoginFormController.php
@@ -6,6 +6,10 @@ class LoginFormController implements Controller
 {
     public function processaRequisicao(): void
     {
+        if(array_key_exists('logado', $_SESSION) && $_SESSION['logado'] === true){
+            header('Location: /');
+            return;
+        }
         require_once __DIR__ . '/../../views/login-form.php';
     }
 }
