f5acc716c818860aa4f0f93bb30542a3626ff5af
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-02-19 15:26:33 -0300

    larevel-formularios-sessoes

--- Laravel-criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel-criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
@@ -8,11 +8,14 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
-        return view('series.index')->with('series', $series);
-   }
+        $mensagemSucesso = session('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
+    }
 
     public function create()
     {
@@ -21,11 +24,31 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        $nomeSerie = $request->input('nome');
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
+        $serie = Serie::create($request->all());
 
-            return redirect('/series');
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
     }
 }
--- Laravel-criando-aplicacao-mvc/controle-series/resources/views/series/index.blade.php
+++ Laravel-criando-aplicacao-mvc/controle-series/resources/views/series/index.blade.php
@@ -1,11 +1,31 @@
-<x-layout title="Séries"></x-layout>
+<x-layout title="Séries">
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
 
-<div class="container">
- <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
-<ul class="list-group">
-    @foreach ($series as $serie)
-    <li class="list-group-item">{{ $serie->nome }}</li>
-    @endforeach
-</ul>
-</div>
+    @isset($mensagemSucesso)
+        <div class="alert alert-success">
+            {{ $mensagemSucesso }}
+        </div>
+    @endisset
 
+    <ul class="list-group">
+        @foreach ($series as $serie)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{ $serie->nome }}
+
+                <span class="d-flex">
+                <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                    E
+                </a>
+
+                <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+            </span>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
